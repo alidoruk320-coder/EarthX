47ed4df828115ec65329ce87b813d1eba191b571
alidoruk320-coder <ali.doruk320@gmail.com>
2026-09-20 17:31:02 +0300

    Support Netlify deployment paths

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import cesium from "vite-plugin-cesium";
 
 export default defineConfig({
-  base: "/EarthX/",          // ← bu satır çok önemli
+  base: "./",
   plugins: [
     react(),
     cesium()
